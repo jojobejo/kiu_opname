03cbf0264b69e6c13c21aca5198ae28c3ff3c18a
Bram <maulanamalik6696@gmail.com>
2023-05-27 08:38:17 +0700

    update

--- application/views/content/admin/dashboard.php
+++ application/views/content/admin/dashboard.php
@@ -30,6 +30,50 @@
                         <div class="row">
                             <div class="col-lg-3 col-6">
                                 <!-- small box -->
+                                <div class="small-box bg-info">
+                                    <div class="inner">
+                                        <?php foreach ($selisihFaktur as $p) : ?>
+                                            <h3><?= $p->total ?></h3>
+                                        <?php endforeach; ?>
+                                        <p>Jumlah Barang</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-bag"></i>
+                                    </div>
+                                    <a href="<?php echo base_url('data_zahir') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+                            <!-- ./col -->
+                            <div class="col-lg-3 col-6">
+                                <!-- small box -->
+                               
+                            </div>
+                            <div class="col-lg-3 col-6">
+                                <!-- small box -->
+                                <div class="small-box bg-danger">
+                                    <div class="inner">
+                                        <?php
+                                        foreach ($selisihFaktur as $s) {
+                                            $match = $s->match;
+                                            $not   = $s->not;
+                                            $total = $s->total;
+
+                                            $vM = ($match / $total) * 100;
+                                            $vN = ($not / $total) * 100;
+                                            $hM =  round((float)$vN, 2);
+                                        } ?>
+                                        <h3><?php echo json_encode($hM) ?><sup style="font-size: 20px">%</sup></h3>
+                                        <p>Persentase Tidak Cocok</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                    </div>
+                                    <a href="<?php echo base_url('quick_count') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+                            <!-- ./col -->
+                            <div class="col-lg-3 col-6">
+                                <!-- small box -->
                                 <div class="small-box bg-warning">
                                     <?php foreach ($totalUser as $s) {
                                         $total = $s->total;
