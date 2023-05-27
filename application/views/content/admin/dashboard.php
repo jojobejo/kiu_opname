bb819add13ffed820208cd1a012e5e01a278ed87
Bram <maulanamalik6696@gmail.com>
2023-05-27 08:36:16 +0700

    update

--- application/views/content/admin/dashboard.php
+++ application/views/content/admin/dashboard.php
@@ -30,24 +30,6 @@
                         <div class="row">
                             <div class="col-lg-3 col-6">
                                 <!-- small box -->
-                                <div class="small-box bg-info">
-                                    <div class="inner">
-                                        <?php foreach ($selisihFaktur as $p) : ?>
-                                            <h3><?= $p->total ?></h3>
-                                        <?php endforeach; ?>
-                                        <p>Jumlah Barang</p>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ion ion-bag"></i>
-                                    </div>
-                                    <a href="<?php echo base_url('data_zahir') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                            <!-- ./col -->
-                         
-                            <!-- ./col -->
-                            <div class="col-lg-3 col-6">
-                                <!-- small box -->
                                 <div class="small-box bg-warning">
                                     <?php foreach ($totalUser as $s) {
                                         $total = $s->total;
