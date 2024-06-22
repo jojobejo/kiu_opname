a5ef53a9f92b144b570614f4bc3f8ec1984f599f
Bram <maulanamalik6696@gmail.com>
2024-06-22 12:29:35 +0700

    update_db_trial

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -24,10 +24,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
- $config['base_url']    = 'https://kiu.co.id/opname/';
+// $config['base_url']    = 'https://kiu.co.id/opname/';
 
-//  $config['base_url'] = 'https://localhost/kiu_opname/';
+// //  $config['base_url'] = 'https://localhost/kiu_opname/';
 
+$root  = "http://" . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url']    = $root;
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -55,7 +58,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']  = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -81,7 +84,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']  = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -411,12 +414,12 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
-$config['cookie_samesite'] 	= 'Lax';
+$config['cookie_prefix']  = '';
+$config['cookie_domain']  = '';
+$config['cookie_path']    = '/';
+$config['cookie_secure']  = FALSE;
+$config['cookie_httponly']   = FALSE;
+$config['cookie_samesite']   = 'Lax';
 
 /*
 |--------------------------------------------------------------------------
