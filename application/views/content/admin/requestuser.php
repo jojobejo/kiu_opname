ad0d99da229c3cbfd03102513f477643928644ad
Bram <maulanamalik6696@gmail.com>
2024-06-26 22:29:06 +0700

    updatemalam

--- application/views/content/admin/requestuser.php
+++ application/views/content/admin/requestuser.php
@@ -29,24 +29,20 @@
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h3 class="card-title">Data barang Zahir</h3>
+                                    <h3 class="card-title">User Request</h3>
                                 </div>
                                 <div>
-                                    <a href="<?= base_url('add_data_zahir') ?>" type="button" class="btn btn-primary m-2 ml-3">
-                                        <i class="fas fa-plus"></i>
-                                        Tambah Data Zahir
-                                    </a>
-
                                     <!-- /.card-header -->
                                     <div class="card-body">
-                                        <table id="dbzahir" class="table table-bordered table-striped">
+                                        <table id="a" class="table table-bordered table-striped">
                                             <thead>
                                                 <tr>
                                                     <th>Kode Barang</th>
                                                     <th>Nama Barang</th>
                                                     <th>Expired Date</th>
                                                     <th>Qty</th>
-                                                    <th>Keterangan</th>
+                                                    <th>stok_box</th>
+                                                    <th>stok_pcs</th>
                                                     <th>Aksi</th>
                                                 </tr>
                                             </thead>
