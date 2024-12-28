0b406f5409bb7262b4f7e4ab13287c3f0bb99e0c
Maulana Malik Ibrahim <maulanamalik6696@gmail.com>
2024-12-29 02:19:50 +0700

    updated_2024123

--- application/controllers/C_Summaryopaname.php
+++ application/controllers/C_Summaryopaname.php
@@ -202,7 +202,7 @@ class C_Summaryopaname extends CI_Controller
             $excel->setActiveSheetIndex(0)->setCellValue('C' . $numrow, $data->nama_barang);
             $excel->setActiveSheetIndex(0)->setCellValue('D' . $numrow, $data->faktur_pending);
             $excel->setActiveSheetIndex(0)->setCellValue('E' . $numrow, $data->saldo_buku);
-            $excel->setActiveSheetIndex(0)->setCellValue('F' . $numrow, $data->qty_b);
+            $excel->setActiveSheetIndex(0)->setCellValue('F' . $numrow, $data->saldo_fisik);
             $excel->setActiveSheetIndex(0)->setCellValue('G' . $numrow, $data->selisih);
             $excel->setActiveSheetIndex(0)->setCellValue('H' . $numrow, $data->hasil);
 
